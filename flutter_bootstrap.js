dcb25dff3ee1baef2bfb662721e755111ac7d465
gracken00 <galo.aguirre.ec@gmail.com>
2025-10-20 16:17:43 -0500

    pushing flutter web app code today again, again

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2969263577"
+    serviceWorkerVersion: "1035005022"
   }
 });
